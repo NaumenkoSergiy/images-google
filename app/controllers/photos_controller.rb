d4a0e94950c0c1dc9bfc3d6be6beb6f3c409c4c9
NaumenkoSergiy <s.naumenko@active-bridge.com>
2015-08-28 17:38:01 +0300

    redirect if session[:code] = nil

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,12 +1,13 @@
 class PhotosController < ApplicationController
+  before_filter :check_authorization_code
+
   def destroy
-    if session[:code]
-      client.photo.destroy(params[:album_id], params[:id])
-      redirect_to root_path
-    else
-      redirect_to "https://accounts.google.com/o/oauth2/auth?scope=http://picasaweb.google.com/data/&redirect_uri=http://localhost:3000/authorization_code&response_type=code&client_id=#{ENV['GOOGLE_CLIENT_ID']}"
-    end
+    client.photo.destroy(params[:album_id], params[:id])
+    redirect_to root_path
+    redirect_to "https://accounts.google.com/o/oauth2/auth?scope=http://picasaweb.google.com/data/&redirect_uri=http://localhost:3000/authorization_code&response_type=code&client_id=#{ENV['GOOGLE_CLIENT_ID']}"
   end
+
+  def new; end
 
   def create
     photo_bin = File.binread(params[:file].tempfile)
@@ -22,5 +23,11 @@ class PhotosController < ApplicationController
     )
 
     redirect_to root_path
+  end
+
+  private
+
+  def check_authorization_code
+    redirect_to "https://accounts.google.com/o/oauth2/auth?scope=http://picasaweb.google.com/data/&redirect_uri=http://localhost:3000/authorization_code&response_type=code&client_id=#{ENV['GOOGLE_CLIENT_ID']}" if session[:code].nil?
   end
 end
