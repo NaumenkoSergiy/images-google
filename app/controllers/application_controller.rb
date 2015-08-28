4ac6ebbe66b58494b58a36d3168ba8cc9c7a1ff6
NaumenkoSergiy <s.naumenko@active-bridge.com>
2015-08-28 20:51:48 +0300

    removed rescue from Exception method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :client
 
-  rescue_from Exception, with: :redirect_if_error
-
   def client
     @client ||= Picasa::Client.new(user_id: current_user.email, access_token: session[:token]['access_token']) if current_user
-  end
-
-  def redirect_if_error
-    redirect_to root_path unless params[:controller] == 'dashboard'
   end
 end
