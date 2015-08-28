9c85bb74c0bb29963274629e73eb895250e86c77
NaumenkoSergiy <s.naumenko@active-bridge.com>
2015-08-28 18:20:44 +0300

    redirect to root path if error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,13 @@ class ApplicationController < ActionController::Base
 
   helper_method :client
 
+  rescue_from Exception, with: :redirect_if_error
+
   def client
     @client ||= Picasa::Client.new(user_id: current_user.email, access_token: session[:token]['access_token']) if current_user
+  end
+
+  def redirect_if_error
+    redirect_to root_path
   end
 end
